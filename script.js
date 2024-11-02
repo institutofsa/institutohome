64818e611477380c6241af6c39b04cec39bc5202
institutofsa <eduardaborgesmelo@gmail.com>
2024-11-02 20:31:54 +0000

    commit03

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function showAgenda() {
     document.getElementById('agendaPage').style.display = 'block';
 }
 
-
 function showWelcome() {
     document.getElementById('agendaPage').style.display = 'none';
     document.getElementById('welcomePage').style.display = 'block';
